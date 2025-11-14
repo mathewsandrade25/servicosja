9272c0b55aa63f781697c841d06095ca12747732
Marcos Ribeiro <131191211+Marcsfic98@users.noreply.github.com>
2025-11-14 17:14:45 -0300

    feat[chatbot] : popup created

--- frontend/src/components/chatbot/chatbot.jsx
+++ frontend/src/components/chatbot/chatbot.jsx
@@ -1,17 +1,45 @@
+import { useState } from 'react'
+
+
 import styles from './chatbot.module.css'
 import {Dialog} from '@mui/material'
+import { FaPaperclip } from "react-icons/fa6";
+import { IoExitOutline } from "react-icons/io5";
 
 
 export default function Chatbot () {
+
+    const [open, setOpen] = useState(false);
+
+    const handleClose = () => {
+        // Lógica para fechar o popup
+        setOpen(!open);
+    }   
+
     return (
         <>
-            <img className={styles.chatIcon} src="/img/chatbot/Icon chat.svg" alt="" />
-            <Dialog open={true}>
+            <img onClick={handleClose} className={styles.chatIcon} src="/img/chatbot/Icon chat.svg" alt="" />
+            <Dialog  className={styles.popupContainer} open={open}>
                <div className={styles.popup}>
-                 <div className={styles.popupMenu}>
-                    <h2>Suporte Serviços Já</h2>
-                    <h2>X</h2>
-                 </div>
+                    <div className={styles.popupMenu}>
+                        <img src="/img/logo/Simbolo.svg" alt="Logo serviços já" />
+                        <h2>Suporte Serviços Já</h2>
+                        <div onClick={() => handleClose()} className={styles.exitIcon}>
+                            <IoExitOutline />
+                        </div>
+                    </div>
+
+                    <div className={styles.popupBody}>
+                        <div className={styles.messageBot}>
+                            <p>Olá! Eu sou o assistente virtual do Serviços Já. Como posso ajudar você hoje?</p>
+                        </div>
+                    </div>
+
+                    <div className={styles.popupFooter}>
+                        <FaPaperclip />
+                        <input type="text" placeholder='Digite sua mensagem...' />
+                        <button>Enviar</button>
+                    </div>
                </div>
 
             </Dialog>
